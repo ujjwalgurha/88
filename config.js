c308ca60d5edf92dbf340964ce66f7912839ebba
ujjwalgurha <87235202+ujjwalgurha@users.noreply.github.com>
2024-01-03 22:43:29 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyDbf82DUTrVWPzphHg4y2a1SbrItWzhYJc",
+    authDomain: "speactagram.firebaseapp.com",
+    databaseURL: "https://speactagram-default-rtdb.firebaseio.com",
+    projectId: "speactagram",
+    storageBucket: "speactagram.appspot.com",
+    messagingSenderId: "700848769774",
+    appId: "1:700848769774:web:4a3f850a21eceec80841ff"
+  };
